098d441dd2ecec639b8d8c44b784dc6133ffabeb
andoni_rodriguez <andoni_rodriguez@epam.com>
2024-03-06 12:22:55 -0600

    implemented DTO for user details

--- springboot-web/src/main/java/com/andoni/springboot/course/webapp/springbootweb/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/andoni/springboot/course/webapp/springbootweb/controllers/UserRestController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andoni.springboot.course.webapp.springbootweb.models.User;
+import com.andoni.springboot.course.webapp.springbootweb.models.dto.UserDTO;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details")
+    @GetMapping("/details-map")
     public Map<String, Object> getDetails() {
         Map<String, Object> body = new HashMap<>();
         User user = new User("John", "Doe");
@@ -22,6 +23,17 @@ public class UserRestController {
         body.put("user", user);
 
         return body;
+    }
+
+    @GetMapping("/details-dto")
+    public UserDTO getDetailsDto() {
+        User user = new User("John", "Doe");
+        UserDTO userDTO = new UserDTO();
+
+        userDTO.setUser(user);
+        userDTO.setTitle("Hello world with DTO");
+
+        return userDTO;
     }
 
 }
