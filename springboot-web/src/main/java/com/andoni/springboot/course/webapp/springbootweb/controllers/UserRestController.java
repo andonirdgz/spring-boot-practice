ba245aa9afb88e15a0a6d76efea3a2378fd2c29b
andoni_rodriguez <andoni_rodriguez@epam.com>
2024-03-06 12:29:05 -0600

    added list users endpoint

--- springboot-web/src/main/java/com/andoni/springboot/course/webapp/springbootweb/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/andoni/springboot/course/webapp/springbootweb/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.andoni.springboot.course.webapp.springbootweb.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +37,16 @@ public class UserRestController {
         userDTO.setTitle("Hello world with DTO");
 
         return userDTO;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        User[] usersArray = { new User("User 1", "Lastname 1"), new User("User 2", "Lastname 2"),
+                new User("User 3", "Lastname 3") };
+
+        List<User> users = new ArrayList<>(Arrays.asList(usersArray));
+
+        return users;
     }
 
 }
